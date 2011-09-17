8dc4145a9bf47e1f5ec5488113dcb98656e3f8b6
harold <harold@mit.edu>
2011-09-17 13:35:40 -0400

    avoid infinite redirects

--- 4sq/4sq.js
+++ 4sq/4sq.js
@@ -1,2 +1,4 @@
-var callback = encodeURIComponent(window.location);
-window.location = "https://foursquare.com/oauth2/authenticate?client_id=YORIDF5MNQMUDY3JPSS22RSI0NTRANA4UURVUMDL4FHARQ3C&response_type=token&redirect_uri=" + callback
+if (!window.location.hash) {
+    var callback = encodeURIComponent(window.location);
+    window.location = "https://foursquare.com/oauth2/authenticate?client_id=YORIDF5MNQMUDY3JPSS22RSI0NTRANA4UURVUMDL4FHARQ3C&response_type=token&redirect_uri=" + callback;
+}
