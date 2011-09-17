bbbfc4e7873da051ebf23a6f8798e133eb817ffe
harold <harold@mit.edu>
2011-09-17 13:31:35 -0400

    simple js redirect

--- 4sq.js
+++ 4sq.js
@@ -1,10 +1,2 @@
-function getQueryString() {
-    var result = {}, queryString = location.search.substring(1),
-			 re = /([^&=]+)=([^&]*)/g, m;
-    while (m = re.exec(queryString)) {
-	result[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
-    }
-    return result;
-}
-
-console.log(getQueryString());
+var callback = encodeURIComponent(window.location);
+window.location = "https://foursquare.com/oauth2/authenticate?client_id=YORIDF5MNQMUDY3JPSS22RSI0NTRANA4UURVUMDL4FHARQ3C&response_type=token&redirect_uri=" + callback
